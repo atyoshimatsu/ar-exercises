b03c07b3f017a64b7b7f2b02236757f51bbe83d1
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-12-15 23:31:11 -0800

    Add Store class validates

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }, presence: true
+  validates :mens_apparel, :womens_apparel, inclusion: { in: [true, false] }
+  validate :must_carry_mens_apparel_or_womens_apparel
+  def must_carry_mens_apparel_or_womens_apparel
+    Store.all.each do |store|
+      if !store.mens_apparel && !store.womens_apparel
+        errors.add(:mens_apparel, "must carry mens apparel or womens apparel")
+        errors.add(:womens_apparel, "must carry mens apparel or womens apparel")
+      end
+    end
+  end
 end
