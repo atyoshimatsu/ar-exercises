af0b6b9c8a2fbb721a3fbaa406598a38e0bd1576
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-12-15 23:30:58 -0800

    Add Employee class validates

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
   belongs_to :stores
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates_associated :stores
 end
