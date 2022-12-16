f39e0a388839d48b0256451854c1ecf0f546c725
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-12-16 18:37:08 +0000

    Modify custom validation

--- lib/store.rb
+++ lib/store.rb
@@ -4,12 +4,13 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }, presence: true
   validates :mens_apparel, :womens_apparel, inclusion: { in: [true, false] }
   validate :must_carry_mens_apparel_or_womens_apparel
+
+  private
+
   def must_carry_mens_apparel_or_womens_apparel
-    Store.all.each do |store|
-      if !store.mens_apparel && !store.womens_apparel
-        errors.add(:mens_apparel, "must carry mens apparel or womens apparel")
-        errors.add(:womens_apparel, "must carry mens apparel or womens apparel")
-      end
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "must carry mens apparel or womens apparel")
+      errors.add(:womens_apparel, "must carry mens apparel or womens apparel")
     end
   end
 end
