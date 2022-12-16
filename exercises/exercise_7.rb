d20affb2e38ffbf3268b174dd3b2ad258f9dc768
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-12-15 23:31:32 -0800

    Create exercise7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@store_id = Employee.find_by(id: 1).store_id
+@store_name = Store.find_by(id: @store_id).name
+puts @store_name
+
+puts "----- enter new store name -----"
+new_store_name = gets.chomp
+
+@new_store = Store.create(name: new_store_name)
+
+puts "----- error messages -----"
+@new_store.errors.full_messages.each do |message|
+  puts message
+end
